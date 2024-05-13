840e65799d23221671b05e73f25cb8b6211d0f45
JagodaBodnar <jagodabodnar90@gmail.com>
2024-05-13 09:41:04 +0200

    Reversing changes to test on vercel

---
+++ src/components/routing/routing.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import { PageNotFound } from "../../components";
+import App from "../../app.tsx";
+
+export const router = createBrowserRouter([
+  {
+    path: "/card/course",
+    element: <App />,
+    errorElement: <PageNotFound />,
+  },
+]);
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {ClerkProvider} from '@clerk/clerk-react'
-import {HashRouter} from "react-router-dom";
-import App from "./app.tsx";
+import {RouterProvider} from "react-router-dom";
+import {router} from "./components/routing/routing.tsx";
 
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -14,9 +14,7 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <HashRouter basename={"/card/course"}>
-        <App/>,
-      </HashRouter>
+      <RouterProvider router={router}/>
     </ClerkProvider>
   </React.StrictMode>,
 )
