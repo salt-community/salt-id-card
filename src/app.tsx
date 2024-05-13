6031d045ba34656abf5c0f2390b1f68bc6e84b33
kevingida <kevingida@gmail.com>
2024-05-13 13:46:15 +0200

    add homepage

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,18 @@
+import { useLocation } from "react-router-dom";
 import "./app.css";
 import { Main } from "./components";
+import { useEffect } from "react";
 
 const App = () => {
+  const course = new URLSearchParams(useLocation().search).get("name");
+  // console.log(date)
+  const startDate = new URLSearchParams(useLocation().search).get("date");
+  useEffect(() => {
+    console.log(course);
+    if (course == null || startDate == null) {
+      console.log("no course");
+    }
+  }, []);
   return (
     <div className="root">
       <Main />
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {ClerkProvider} from '@clerk/clerk-react'
-import {HashRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./app.tsx";
+import { PageNotFound } from "./components";
 
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  throw new Error("Missing Publishable Key");
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+export const router = createBrowserRouter([
+  {
+    path: "/card/course/",
+    element: <App />,
+    errorElement: <PageNotFound />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <HashRouter basename={"/card/course"}>
-        <App/>
-      </HashRouter>
+      <RouterProvider router={router} />
     </ClerkProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
