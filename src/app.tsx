e2337c1b64b2fb10af8cc08c46a1083e4902e51d
kevingida <kevingida@gmail.com>
2024-05-13 14:29:58 +0200

    change app to function component

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,18 @@
-import { useLocation } from "react-router-dom";
 import "./app.css";
+import { useLocation } from "react-router-dom";
+import { Main } from "./components";
 
-const App = () => {
+function App() {
   const course = new URLSearchParams(useLocation().search).get("name");
-  // console.log(date)
   const startDate = new URLSearchParams(useLocation().search).get("date");
-  // useEffect(() => {
-  //   console.log(course);
-  //   if (course == null || startDate == null) {
-  //     console.log("no course");
-  //   }
-  // }, []);
+
   return (
-    <div className="root">
+    <>
       <h2>{course}</h2>
       <h2>{startDate}</h2>
-      {/* <Main /> */}
-    </div>
+      <Main />
+    </>
   );
-};
+}
+
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { ClerkProvider } from "@clerk/clerk-react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./app.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
