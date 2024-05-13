fffabe4f9571fc49bea0da7bee833b35c8d0f29e
kevingida <kevingida@gmail.com>
2024-05-13 10:31:28 +0200

    Try new routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,11 @@
+import { Outlet } from "react-router-dom";
 import "./app.css";
-import {Main} from "./components";
+import { Main } from "./components";
 
 const App = () => {
   return (
     <div className="root">
-      <Main/>
+      <Outlet />
     </div>
   );
 };
--- src/components/routing/routing.tsx
+++ src/components/routing/routing.tsx
@@ -1,26 +1,22 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { PageNotFound } from "../../components";
 import App from "../../app.tsx";
+import { Home } from "../../pages/home.tsx";
+import { Main } from "../../pages/main.tsx";
 
-// export const router = createBrowserRouter([
-//   {
-//     path: "/card/course",
-//     element: <App />,
-//     errorElement: <PageNotFound />,
-//   },
-// ]);
-
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route
-      path="/card/course" // <-- "/presentation"
-      element={<App />}
-      errorElement={<PageNotFound />}
-    ></Route>
-  ),
-  { basename: "/" }
-);
+export const router = createBrowserRouter([
+  {
+    path: "/vite-react-router/",
+    element: <App />,
+    children: [
+      {
+        path: "/vite-react-router/",
+        element: <Home />,
+      },
+      {
+        path: "/vite-react-router/contact",
+        element: <Main />,
+      },
+    ],
+  },
+]);
