91538b85ee9e8a34a9df9c51f3d786a9d0e50584
TobiasBlankJohansson <Tobias@annito.se>
2025-02-19 12:41:11 +0100

    feat:add CardHeadProp in CardHead and set span card-head__status to use status

--- src/components/id-card/id-card-head/card-head.tsx
+++ src/components/id-card/id-card-head/card-head.tsx
@@ -1,12 +1,16 @@
 import saltLogo from "../../../assets/salt-logo-dark.svg";
 import "./card-head.css"
 
-export const CardHead = () => {
+type CardHeadProp = {
+  status: string,
+}
+
+export const CardHead = ({status}:CardHeadProp) => {
   return (
     <section className="card-section__head">
       <img src={saltLogo} alt="salt logo" className="card-head__logo"/>
       <span className="card-head__name">Applied Technology STHLM AB</span>
-      <span className="card-head__status">student</span>
+      <span className="card-head__status">{status}</span>
     </section>
   );
 };
