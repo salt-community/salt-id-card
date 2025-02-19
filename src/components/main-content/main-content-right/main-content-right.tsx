80d471b5d2c11d12bfa1d0b56fa13bcbd8a279d5
TobiasBlankJohansson <Tobias@annito.se>
2025-02-19 12:56:39 +0100

    refa:refactor MainContentRight by removing tooltip and url check

--- src/components/main-content/main-content-right/main-content-right.tsx
+++ src/components/main-content/main-content-right/main-content-right.tsx
@@ -1,18 +1,13 @@
-import { IdCard, CtaButton, Tooltip } from "../../../components";
-import { handlePrint, isDisabled } from "../../../utils/utils.ts";
+import { IdCard, CtaButton } from "../../../components";
+import { handlePrint } from "../../../utils/utils.ts";
 import { useEffect, useRef, useState } from "react";
 import "./main-content-right.css";
-import { useLocation } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
 import { User } from "../../../types.ts";
 import { getSaltCardData } from "../../../api/notion.ts";
 
 export const MainContentRight = () => {
   const printRef = useRef<HTMLDivElement>(null);
-  const date = new URLSearchParams(useLocation().search).get("date");
-  const name = new URLSearchParams(useLocation().search).get("name");
-  const location = new URLSearchParams(useLocation().search).get("location");
-  const disabled = isDisabled(date!, name!, location!);
 
   const { user } = useUser();
   const [userData, setUserData] = useState<User>({
@@ -47,15 +42,13 @@ export const MainContentRight = () => {
     userData && (
       <div className="main-content-right__wrapper">
         <IdCard userData={userData!} ref={printRef} />
-        <Tooltip content="Don't mess with the url!" disabled={disabled}>
           <CtaButton
             onClick={() => handlePrint(userData!, printRef)}
             variant="info"
-            disabled={disabled}
+            disabled={false}
           >
             Download my ID
           </CtaButton>
-        </Tooltip>
       </div>
     )
   );
