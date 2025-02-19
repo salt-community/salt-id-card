20b4a74d86020febb9980af536ed00e431ffd26f
TobiasBlankJohansson <Tobias@annito.se>
2025-02-19 13:50:44 +0100

    refa:remove import { PageNotFound } from ../components;

--- src/routing/router.tsx
+++ src/routing/router.tsx
@@ -1,11 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../app.tsx";
-import { PageNotFound } from "../components";
 
 export const router = createBrowserRouter([
   {
     path: "/salt-id-card/",
     element: <App />,
-    errorElement: <PageNotFound />,
+    errorElement: <></>,
   },
 ]);
