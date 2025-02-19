e75977a78828ce2295b753cf1b298df18faeada3
TobiasBlankJohansson <Tobias@annito.se>
2025-02-19 15:10:13 +0100

    feat:add IdCard in validate

--- src/pages/validate.tsx
+++ src/pages/validate.tsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { User } from "../types";
+import { getSaltCardData } from "../api/notion";
+import { IdCard } from "../components";
 
 export function Validate() {
   const email = new URLSearchParams(useLocation().search).get("email");
-  return <p>{email}</p>;
+  const [userData, setUserData] = useState<User>({
+    email: "loading...",
+    name: "loading...",
+    course: "loading...",
+    endDate: "loading...",
+    status: "loading...",
+    image: "loading...",
+  });
+
+  useEffect(() => {
+    const execute = async () => {
+      if (email) {
+        const idCardData = await getSaltCardData(email);
+        setUserData({
+          email: email,
+          name: idCardData.name,
+          course: idCardData.course,
+          endDate: idCardData.endDate,
+          status: idCardData.status,
+          image: "",
+        });
+      }
+    };
+    execute();
+  }, [email]);
+  
+  return <IdCard userData={userData} />;
 }
