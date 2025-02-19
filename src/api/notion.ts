ad6bc919a2b6d5baa9691be85ccc6d46b114017e
TobiasBlankJohansson <Tobias@annito.se>
2025-02-19 15:38:24 +0100

    fix:fix mock data in  notion

--- src/api/notion.ts
+++ src/api/notion.ts
@@ -1,12 +1,14 @@
 import { User } from "../types";
 
-export async function getSaltCardData(email: string | undefined): Promise<User> {
+export async function getSaltCardData(
+  email: string | undefined
+): Promise<User> {
   return {
     name: "Tobias Johansson",
     course: "jfs-sthlm-2024-09-06",
     endDate: "2025-06-01",
     email: email,
     status: "PGP",
-    image: "https://github.com/TobiasBlankJohansson.png"
+    image: "https://avatars.githubusercontent.com/TobiasBlankJohansson",
   };
 }
