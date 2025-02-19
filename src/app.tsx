1412bf3d5cca82b7c25a8359be2157cc003af9cf
TobiasBlankJohansson <Tobias@annito.se>
2025-02-19 12:58:39 +0100

    refa:refactor App to not check the url

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,8 @@
 import "./app.css";
-import { PageNotFound } from "./components";
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
 import { Authentication } from "./components/authentication";
-import { isDisabled } from "./utils/utils";
 
 function App() {
-  const course = new URLSearchParams(useLocation().search).get("name");
-  const date = new URLSearchParams(useLocation().search).get("date");
-  const location = new URLSearchParams(useLocation().search).get("location");
-
-  const [missingParams, setMissingParams] = useState(false);
-
-  useEffect(() => {
-    if (course == null || date == null || location == null) {
-      setMissingParams(true);
-    } else if (isDisabled(date!, course!, location!)) {
-      setMissingParams(true);
-    }
-  }, [course, date, location]);
-
-  return missingParams ? (
-    <PageNotFound />
-  ) : (
+  return (
     <div className="root">
       <Authentication />
     </div>
