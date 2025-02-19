802893405d5bf53fdfa06f1397f65b0ef1589e2e
TobiasBlankJohansson <Tobias@annito.se>
2025-02-19 12:16:37 +0100

    faet:add getSaltCardData that returns mock data

--- src/api/notion.ts
+++ src/api/notion.ts
@@ -1,3 +1,5 @@
+import { IdCard } from "../components";
+
 type IdCard = {
   name: string;
   course: string;
@@ -5,3 +7,13 @@ type IdCard = {
   email: string;
   status: string;
 };
+
+export function getSaltCardData(email: string): IdCard {
+  return {
+    name: "Tobias Johansson",
+    course: "jfs-sthlm-2024-09-06",
+    endDate: "2025-06-01",
+    email: "tobias.johansson@appliedtechnology.se",
+    status: "PGP",
+  };
+}
