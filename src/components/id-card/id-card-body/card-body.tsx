7669f08a40751eba17985d7ad80a2837d75c7751
TobiasBlankJohansson <Tobias@annito.se>
2025-02-19 12:44:37 +0100

    faeat:add course and endDate to CardBodyProp and use the valuse in the dl

--- src/components/id-card/id-card-body/card-body.tsx
+++ src/components/id-card/id-card-body/card-body.tsx
@@ -1,37 +1,25 @@
-import './card-body.css'
-import {useLocation} from "react-router-dom";
+import "./card-body.css";
+import { useLocation } from "react-router-dom";
 
 type CardBodyProp = {
-  image: string,
-  name: string | null
-}
-export const CardBody = ({image, name}: CardBodyProp) => {
-  const course = new URLSearchParams(useLocation().search).get("name");
-  const date = new URLSearchParams(useLocation().search).get("date")
-  const location = new URLSearchParams(useLocation().search).get("location")
-
-  const validUntil = () => {
-    if(date){
-      const splitDate = date.split("-");
-      splitDate[0] = `${+splitDate[0] + 2}`;
-      return splitDate.join("-");
-    }
-  };
+  image: string;
+  name: string | null;
+  course: string;
+  endDate: string;
+};
+export const CardBody = ({ image, name, course, endDate }: CardBodyProp) => {
   return (
     <section className="card-section__body">
       <div className="card-body__img">
-        <img
-          src={image}
-          alt="user picture"
-        />
+        <img src={image} alt="user picture" />
       </div>
       <dl className="card-body__info">
         <dt>Name</dt>
         <dd>{name}</dd>
         <dt>Course</dt>
-        <dd>{date && `${course}-${location}-${date}`}</dd>
+        <dd>{course}</dd>
         <dt>Valid until</dt>
-        <dd>{date && validUntil()}</dd>
+        <dd>{endDate}</dd>
       </dl>
     </section>
   );
