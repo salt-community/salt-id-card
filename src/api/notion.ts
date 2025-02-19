3a67ad1ca58ef17aea520d3acc39e0d1af4e5cb3
TobiasBlankJohansson <Tobias@annito.se>
2025-02-19 15:15:40 +0100

    feat:add image in getSaltCardData

--- src/api/notion.ts
+++ src/api/notion.ts
@@ -1,11 +1,12 @@
-import { IdCard } from "../types";
+import { User } from "../types";
 
-export async function getSaltCardData(email: string | undefined): Promise<IdCard> {
+export async function getSaltCardData(email: string | undefined): Promise<User> {
   return {
     name: "Tobias Johansson",
     course: "jfs-sthlm-2024-09-06",
     endDate: "2025-06-01",
     email: email,
     status: "PGP",
+    image: "https://github.com/TobiasBlankJohansson.png"
   };
 }
