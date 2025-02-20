6a388e3d088b5f84ebaf09251742bd41cceff7e1
TobiasBlankJohansson <Tobias@annito.se>
2025-02-20 15:07:47 +0100

    feat:add backend

---
+++ backend/src/test/java/salt/dev/saltidcard/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package salt.dev.saltidcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
