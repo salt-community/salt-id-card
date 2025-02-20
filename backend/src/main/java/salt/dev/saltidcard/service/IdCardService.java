00b86e73616847be571bd498df256f931f641336
TobiasBlankJohansson <Tobias@annito.se>
2025-02-20 20:35:33 +0100

    feat:add return user in getIdCard with mock data

--- backend/src/main/java/salt/dev/saltidcard/service/IdCardService.java
+++ backend/src/main/java/salt/dev/saltidcard/service/IdCardService.java
@@ -1,10 +1,15 @@
 package salt.dev.saltidcard.service;
 
 import org.springframework.stereotype.Service;
+import salt.dev.saltidcard.model.User;
 
 @Service
 public class IdCardService {
-    public String getIdCard(){
-        return null;
+    public User getIdCard(){
+        return new User("Tobias Johansson",
+                "jfs-sthlm-2024-09-06",
+                "2025-06-01",
+                "PGP",
+                "https://avatars.githubusercontent.com/TobiasBlankJohansson");
     }
 }
