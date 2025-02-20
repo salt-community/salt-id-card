20dd2fd3f599a8192845f5337cb0b2409f85e864
TobiasBlankJohansson <Tobias@annito.se>
2025-02-20 20:26:31 +0100

    feat:add userDto in getIdCard with mock data

--- backend/src/main/java/salt/dev/saltidcard/controller/Controller.java
+++ backend/src/main/java/salt/dev/saltidcard/controller/Controller.java
@@ -4,14 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import salt.dev.saltidcard.controller.dtos.UserDto;
 
 @RestController
 @RequestMapping("api/v1/id-card")
 public class Controller {
 
     @GetMapping("{email}")
-    public String getIdCard(@PathVariable String email) {
-        return null;
+    public UserDto getIdCard(@PathVariable String email) {
+        return new UserDto("Tobias Johansson",
+                "jfs-sthlm-2024-09-06",
+                "2025-06-01",
+                "PGP",
+                "https://avatars.githubusercontent.com/TobiasBlankJohansson");
     }
-
 }
