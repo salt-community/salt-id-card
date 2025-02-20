f530af0a01a08e9cac59e7c3b883f0ec07f12e1d
TobiasBlankJohansson <Tobias@annito.se>
2025-02-20 20:38:56 +0100

    feat:add service in Controller

--- backend/src/main/java/salt/dev/saltidcard/controller/Controller.java
+++ backend/src/main/java/salt/dev/saltidcard/controller/Controller.java
@@ -5,10 +5,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import salt.dev.saltidcard.controller.dtos.UserDto;
+import salt.dev.saltidcard.service.IdCardService;
 
 @RestController
 @RequestMapping("api/v1/id-card")
 public class Controller {
+
+    public final IdCardService idCardService;
+
+    public Controller(IdCardService idCardService) {
+        this.idCardService = idCardService;
+    }
 
     @GetMapping("{email}")
     public UserDto getIdCard(@PathVariable String email) {
