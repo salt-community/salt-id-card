1d14e84040819d7bd7b7b0b199b1b76ed15c508d
TobiasBlankJohansson <Tobias@annito.se>
2025-02-20 20:24:35 +0100

    feat:add userdto

---
+++ backend/src/main/java/salt/dev/saltidcard/controller/dtos/userDto.java
@@ -0,0 +1,10 @@
+package salt.dev.saltidcard.controller.dtos;
+
+public record userDto(
+        String name,
+        String course,
+        String endDate,
+        String status,
+        String image
+) {
+}
