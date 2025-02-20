1906a98e830771777157235ac6cdf153499f0061
TobiasBlankJohansson <Tobias@annito.se>
2025-02-20 14:23:29 +0100

    feat:add logic for validate-content-middle

--- src/components/validate-content/validate-content-middle/validate-content-middle.tsx
+++ src/components/validate-content/validate-content-middle/validate-content-middle.tsx
@@ -1,3 +1,45 @@
-export const ValidateContentMiddle = () => {
+import { useLocation } from "react-router-dom";
+import { IdCard } from "../../id-card";
+import { Title } from "../../title";
+import { useEffect, useState } from "react";
+import { User } from "../../../types";
+import { getSaltCardData } from "../../../api/notion";
+import "./validate-content-middle.css";
 
-}
+export const ValidateContentMiddle = () => {
+  const email = new URLSearchParams(useLocation().search).get("email");
+  const [userData, setUserData] = useState<User>({
+    email: "loading...",
+    name: "loading...",
+    course: "loading...",
+    endDate: "loading...",
+    status: "loading...",
+    image: "loading...",
+  });
+
+  useEffect(() => {
+    const execute = async () => {
+      if (email) {
+        const idCardData = await getSaltCardData(email);
+        setUserData({
+          email: email,
+          name: idCardData.name,
+          course: idCardData.course,
+          endDate: idCardData.endDate,
+          status: idCardData.status,
+          image: idCardData.image,
+        });
+      }
+    };
+    execute();
+  }, [email]);
+
+  return (
+    <section className="validate__section-middle">
+      <Title content="This salt id-card is valid" />
+      <div className="validate__section-middle__wrapper">
+        <IdCard userData={userData} />
+      </div>
+    </section>
+  );
+};
--- src/components/validate-content/validate-content.tsx
+++ src/components/validate-content/validate-content.tsx
@@ -1,54 +1,15 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { User } from "../../types";
-import { getSaltCardData } from "../../api/notion";
-import { Logo } from "../logo";
-import { Title } from "../title";
-import { IdCard } from "../id-card";
-import { CtaButton } from "../button";
 import "./validate-content.css";
 import { Footer } from "../footer";
 import { ValidateContentTop } from "./validate-content-top";
+import { ValidateContentMiddle } from "./validate-content-middle";
 
 export const ValidateContent = () => {
-  const email = new URLSearchParams(useLocation().search).get("email");
-  const [userData, setUserData] = useState<User>({
-    email: "loading...",
-    name: "loading...",
-    course: "loading...",
-    endDate: "loading...",
-    status: "loading...",
-    image: "loading...",
-  });
-
-  useEffect(() => {
-    const execute = async () => {
-      if (email) {
-        const idCardData = await getSaltCardData(email);
-        setUserData({
-          email: email,
-          name: idCardData.name,
-          course: idCardData.course,
-          endDate: idCardData.endDate,
-          status: idCardData.status,
-          image: idCardData.image,
-        });
-      }
-    };
-    execute();
-  }, [email]);
-
   return (
     <>
       <div className="validate">
         <section className="validate__content">
           <ValidateContentTop/>
-          <section className="validate__section-middle">
-            <Title content="This salt id-card is valid" />
-            <div className="validate__section-middle__wrapper">
-              <IdCard userData={userData} />
-            </div>
-          </section>
+          <ValidateContentMiddle/>
         </section>
         <section className="validate__section-bottom">
           <Footer />
