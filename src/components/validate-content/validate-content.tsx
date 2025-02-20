39fea4d165086c212851cc5106ca05ee83f5231c
TobiasBlankJohansson <Tobias@annito.se>
2025-02-20 11:30:05 +0100

    feat:add css for validate-content

--- src/components/validate-content/validate-content.tsx
+++ src/components/validate-content/validate-content.tsx
@@ -6,6 +6,7 @@ import { Logo } from "../logo";
 import { Title } from "../title";
 import { IdCard } from "../id-card";
 import { CtaButton } from "../button";
+import "./validate-content.css";
 
 export const ValidateContent = () => {
   const email = new URLSearchParams(useLocation().search).get("email");
@@ -38,21 +39,25 @@ export const ValidateContent = () => {
   return (
     <>
       <div className="validate">
-        <section className="validate__section-top">
-          <Logo />
-          <CtaButton
-            variant="primary"
-            onClick={() => window.open("https://salt.dev/contact/", "_blank")}
-          >
-            Contact
-          </CtaButton>
-        </section>
-        <section className="validate__section-middel">
-          <Title content="This salt id-card is valid" />
-          <IdCard userData={userData} />
+        <section className="validate__content">
+          <section className="validate__section-top">
+            <Logo />
+            <CtaButton
+              variant="primary"
+              onClick={() => window.open("https://salt.dev/contact/", "_blank")}
+            >
+              Contact
+            </CtaButton>
+          </section>
+          <section className="validate__section-middle">
+            <Title content="This salt id-card is valid" />
+            <div className="validate__section-middle__wrapper">
+              <IdCard userData={userData} />
+            </div>
+          </section>
         </section>
         <section className="validate__section-bottom">
-          <p>Get in contact with us at Salt</p>
+          <p>SALT (School of Applied Technology) is a leading provider of innovative tech education, with a mission to accelerate growth in the tech industry by supplying the market with an unlimited pipeline of top-talented software developers from diverse backgrounds. Through intensive 13-week boot camps, SALT offers a “try and hire” model for skills matching, as well as specialized programs for reskilling and upskilling, enabling businesses to seamlessly integrate and develop skilled developers.</p>
         </section>
       </div>
     </>
