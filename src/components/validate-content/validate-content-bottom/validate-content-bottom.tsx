035899b99088403e8dfe7a7420995873ec6fadc9
TobiasBlankJohansson <Tobias@annito.se>
2025-02-20 14:26:43 +0100

    feat:add logic for validate-content-bottom

--- src/components/validate-content/validate-content-bottom/validate-content-bottom.tsx
+++ src/components/validate-content/validate-content-bottom/validate-content-bottom.tsx
@@ -1,3 +1,10 @@
-export const ValidateContentBottom = () => {
+import { Footer } from "../../footer";
+import "./validate-content-bottom.css";
 
-}
+export const ValidateContentBottom = () => {
+  return (
+    <section className="validate__section-bottom">
+      <Footer />
+    </section>
+  );
+};
--- src/components/validate-content/validate-content.tsx
+++ src/components/validate-content/validate-content.tsx
@@ -1,19 +1,17 @@
 import "./validate-content.css";
-import { Footer } from "../footer";
 import { ValidateContentTop } from "./validate-content-top";
 import { ValidateContentMiddle } from "./validate-content-middle";
+import { ValidateContentBottom } from "./validate-content-bottom";
 
 export const ValidateContent = () => {
   return (
     <>
       <div className="validate">
         <section className="validate__content">
-          <ValidateContentTop/>
-          <ValidateContentMiddle/>
+          <ValidateContentTop />
+          <ValidateContentMiddle />
         </section>
-        <section className="validate__section-bottom">
-          <Footer />
-        </section>
+        <ValidateContentBottom />
       </div>
     </>
   );
