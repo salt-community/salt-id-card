5121e60aa1d594e7ffb0249c3f68ab2b9a1f3d95
TobiasBlankJohansson <Tobias@annito.se>
2025-02-20 20:21:35 +0100

    feat:add getIdCard

--- backend/src/main/java/salt/dev/saltidcard/controller/controller.java
+++ backend/src/main/java/salt/dev/saltidcard/controller/controller.java
@@ -1,4 +1,17 @@
 package salt.dev.saltidcard.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/id-card")
 public class controller {
+
+    @GetMapping("{email}")
+    public String getIdCard(@PathVariable String email) {
+        return null;
+    }
+
 }
