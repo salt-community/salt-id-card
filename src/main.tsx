e672610a73412f762c1abb51f73d9c7491b54333
TobiasBlankJohansson <tobias@annito.se>
2025-02-21 09:09:31 +0100

    refa:remove strict react

--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <RouterProvider router={router}/>
     </ClerkProvider>
-  </React.StrictMode>
 );
