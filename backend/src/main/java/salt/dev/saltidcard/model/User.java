d823fe6bc7c732631833230692d2c4d60e0a7cf7
TobiasBlankJohansson <tobias@annito.se>
2025-02-21 10:22:45 +0100

    fix:fix lombok

--- backend/src/main/java/salt/dev/saltidcard/model/User.java
+++ backend/src/main/java/salt/dev/saltidcard/model/User.java
@@ -2,11 +2,13 @@ package salt.dev.saltidcard.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class User {
-    String name, course, endDate, status, image;
+    private String name, course, endDate, status, image;
 }
