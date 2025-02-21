815e2d556836e5109d3f4650d37c202edb1c8771
TobiasBlankJohansson <tobias@annito.se>
2025-02-21 10:28:14 +0100

    feat:add CrossOrigin in Controller

--- backend/src/main/java/salt/dev/saltidcard/controller/Controller.java
+++ backend/src/main/java/salt/dev/saltidcard/controller/Controller.java
@@ -1,14 +1,12 @@
 package salt.dev.saltidcard.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import salt.dev.saltidcard.controller.dtos.UserDto;
 import salt.dev.saltidcard.service.IdCardService;
 
 @RestController
 @RequestMapping("api/v1/id-card")
+@CrossOrigin
 public class Controller {
 
     public final IdCardService service;
