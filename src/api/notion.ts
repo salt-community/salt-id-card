4df3a0912afb50122c58fac920f91389ed2763f1
TobiasBlankJohansson <tobias@annito.se>
2025-02-21 10:27:01 +0100

    feat:add fetch to backend in getSaltCardData

--- src/api/notion.ts
+++ src/api/notion.ts
@@ -1,8 +1,15 @@
 import { User } from "../types";
 
+const BACKEND_PATH = import.meta.env.VITE_BACKEND_PATH;
+
 export async function getSaltCardData(
   email: string | undefined
 ): Promise<User> {
+  const result = await fetch(BACKEND_PATH + email);
+  if (!result.ok) {
+    throw new Error();
+  }
+  return (await result.json()) as User;
   return {
     name: "Tobias Johansson",
     course: "jfs-sthlm-2024-09-06",
