5532a52cf4a642f005906be1ea7c59b9afb7325e
TobiasBlankJohansson <Tobias@annito.se>
2025-02-25 11:05:22 +0100

    feat:add getIdCardPrivateEmail and getIdCardUuid in service

--- backend/src/main/java/salt/dev/saltidcard/service/IdCardService.java
+++ backend/src/main/java/salt/dev/saltidcard/service/IdCardService.java
@@ -3,9 +3,25 @@ package salt.dev.saltidcard.service;
 import org.springframework.stereotype.Service;
 import salt.dev.saltidcard.model.User;
 
+import java.util.UUID;
+
 @Service
 public class IdCardService {
-    public User getIdCard(String email){
+    public User getIdCardEmail(String email){
+        return new User("Tobias Johansson",
+                "jfs-sthlm-2024-09-06",
+                "2025-06-01",
+                "PGP",
+                "https://avatars.githubusercontent.com/TobiasBlankJohansson");
+    }
+    public User getIdCardPrivateEmail(String privateEmail){
+        return new User("Tobias Johansson",
+                "jfs-sthlm-2024-09-06",
+                "2025-06-01",
+                "PGP",
+                "https://avatars.githubusercontent.com/TobiasBlankJohansson");
+    }
+    public User getIdCardUuid(UUID uuid){
         return new User("Tobias Johansson",
                 "jfs-sthlm-2024-09-06",
                 "2025-06-01",
