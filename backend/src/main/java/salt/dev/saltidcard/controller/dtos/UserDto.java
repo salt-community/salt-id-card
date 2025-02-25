4d4f3f3a31f693f457d17486e1bb4b0478df3d6b
TobiasBlankJohansson <Tobias@annito.se>
2025-02-25 11:10:28 +0100

    faet:update userdto

--- backend/src/main/java/salt/dev/saltidcard/controller/dtos/UserDto.java
+++ backend/src/main/java/salt/dev/saltidcard/controller/dtos/UserDto.java
@@ -3,13 +3,14 @@ package salt.dev.saltidcard.controller.dtos;
 import salt.dev.saltidcard.model.User;
 
 public record UserDto(
+        String uuid,
         String name,
         String course,
         String endDate,
-        String status,
+        String email,
         String image
 ) {
     public static UserDto toModel(User user) {
-        return new UserDto(user.getName(), user.getCourse(), user.getEndDate(), user.getStatus(), user.getImage());
+        return new UserDto(user.getUuid() ,user.getName(), user.getCourse(), user.getEndDate(), user.getEmail(), user.getImage());
     }
 }
