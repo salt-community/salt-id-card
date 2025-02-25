2955fd3aacbbb2115f5fb56d8fc837b7225ceea3
TobiasBlankJohansson <Tobias@annito.se>
2025-02-25 11:11:35 +0100

    feat:add updated mock data

--- backend/src/main/java/salt/dev/saltidcard/service/IdCardService.java
+++ backend/src/main/java/salt/dev/saltidcard/service/IdCardService.java
@@ -8,24 +8,27 @@ import java.util.UUID;
 @Service
 public class IdCardService {
     public User getIdCardEmail(String email){
-        return new User("Tobias Johansson",
+        return new User("4c51571d-7244-478c-9fff-9ec8941f0094",
+                "Tobias Johansson",
                 "jfs-sthlm-2024-09-06",
-                "2025-06-01",
-                "PGP",
+                "2026-09-06",
+                "tobias.johansson@appliedtechnology.se",
                 "https://avatars.githubusercontent.com/TobiasBlankJohansson");
     }
     public User getIdCardPrivateEmail(String privateEmail){
-        return new User("Tobias Johansson",
+        return new User("4c51571d-7244-478c-9fff-9ec8941f0094",
+                "Tobias Johansson",
                 "jfs-sthlm-2024-09-06",
-                "2025-06-01",
-                "PGP",
+                "2026-09-06",
+                "tobias.johansson@appliedtechnology.se",
                 "https://avatars.githubusercontent.com/TobiasBlankJohansson");
     }
     public User getIdCardUuid(UUID uuid){
-        return new User("Tobias Johansson",
+        return new User("4c51571d-7244-478c-9fff-9ec8941f0094",
+                "Tobias Johansson",
                 "jfs-sthlm-2024-09-06",
-                "2025-06-01",
-                "PGP",
+                "2026-09-06",
+                "tobias.johansson@appliedtechnology.se",
                 "https://avatars.githubusercontent.com/TobiasBlankJohansson");
     }
 }
