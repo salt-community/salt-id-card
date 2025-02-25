6bb16b2c01a790efd429e17594699f862dace25c
TobiasBlankJohansson <Tobias@annito.se>
2025-02-25 08:04:14 +0100

    refa:change status in id-card to student

--- src/components/id-card/id-card-head/card-head.tsx
+++ src/components/id-card/id-card-head/card-head.tsx
@@ -1,16 +1,12 @@
 import saltLogo from "../../../assets/salt-logo-dark.svg";
-import "./card-head.css"
+import "./card-head.css";
 
-type CardHeadProp = {
-  status: string,
-}
-
-export const CardHead = ({status}:CardHeadProp) => {
+export const CardHead = () => {
   return (
     <section className="card-section__head">
-      <img src={saltLogo} alt="salt logo" className="card-head__logo"/>
+      <img src={saltLogo} alt="salt logo" className="card-head__logo" />
       <span className="card-head__name">Applied Technology STHLM AB</span>
-      <span className="card-head__status">{status}</span>
+      <span className="card-head__status">Student</span>
     </section>
   );
 };
--- src/components/id-card/id-card.tsx
+++ src/components/id-card/id-card.tsx
@@ -1,19 +1,27 @@
 import "./id-card.css";
-import {User} from "../../types.ts";
-import React, {forwardRef} from "react";
-import {CardHead} from "./id-card-head";
-import {CardBody} from "./id-card-body";
-import {CardFooter} from "./id-card-footer";
+import { User } from "../../types.ts";
+import React, { forwardRef } from "react";
+import { CardHead } from "./id-card-head";
+import { CardBody } from "./id-card-body";
+import { CardFooter } from "./id-card-footer";
 
 type Props = {
-  userData: User
-}
-export const IdCard = forwardRef(function IdCard(props: Props, ref: React.ForwardedRef<HTMLDivElement>) {
-  const {userData} = props;
+  userData: User;
+};
+export const IdCard = forwardRef(function IdCard(
+  props: Props,
+  ref: React.ForwardedRef<HTMLDivElement>
+) {
+  const { userData } = props;
   return (
     <div className="card-wrapper" ref={ref}>
-      <CardHead status={userData.status} />
-      <CardBody image={userData.image} name={userData.name} course={userData.course} endDate={userData.endDate}/>
+      <CardHead />
+      <CardBody
+        image={userData.image}
+        name={userData.name}
+        course={userData.course}
+        endDate={userData.endDate}
+      />
       <CardFooter email={userData.email} />
     </div>
   );
