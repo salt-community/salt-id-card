a4709b85ad0c967a23408a91b2df1eafbe8afe1b
TobiasBlankJohansson <Tobias@annito.se>
2025-02-25 10:51:12 +0100

    feat:add getSaltDataPrivateEmail in notion api

--- src/api/notion.ts
+++ src/api/notion.ts
@@ -20,6 +20,24 @@ export async function getSaltDataEmail(
   return (await result.json()) as User;
 }
 
+export async function getSaltDataPrivateEmail(
+  privateEmail: string
+): Promise<User> {
+  return {
+    uuid: "4c51571d-7244-478c-9fff-9ec8941f0094",
+    name: "Tobias Johansson",
+    course: "jfs-sthlm-2024-09-06",
+    endDate: "2026-09-06",
+    email: "tobias.johansson@appliedtechnology.se",
+    image: "https://avatars.githubusercontent.com/TobiasBlankJohansson",
+  };
+  const result = await fetch(BACKEND_PATH + privateEmail);
+  if (!result.ok) {
+    throw new Error();
+  }
+  return (await result.json()) as User;
+}
+
 export async function getSaltDataUuid(uuid: string | undefined): Promise<User> {
   return {
     uuid: "4c51571d-7244-478c-9fff-9ec8941f0094",
