f356dec1735b2e9dd0e7208a951fc89c65eb37ad
TobiasBlankJohansson <Tobias@annito.se>
2025-02-25 08:36:30 +0100

    feat:add content in profile

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,9 +1,25 @@
 import { User } from "../../types";
+import { CtaButton } from "../button";
+import { Subtitle } from "../subtitle";
+import { Title } from "../title";
+import image from "../../assets/mail.png"
 
 type Props = {
   userData: User;
 };
 
 export function Profile({ userData }: Props) {
-  return <></>;
+  return (
+    <div className="profile">
+      <img src={userData.image} alt="Id-card image" />
+      <Title content={userData.name}></Title>
+      <Subtitle content={userData.course}></Subtitle>
+      <CtaButton
+        variant="primary"
+        onClick={() => window.open("mailto:" +userData.email, "_blank")}
+      >
+        <img src={image} alt="" />
+      </CtaButton>
+    </div>
+  );
 }
