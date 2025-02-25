5b11b1cc77113ae5ef624ce8282ea4aadc1009c5
TobiasBlankJohansson <Tobias@annito.se>
2025-02-25 11:03:51 +0100

    feat:add getIdCardFromPrivateEmail and getIdCardFromUuid

--- backend/src/main/java/salt/dev/saltidcard/controller/Controller.java
+++ backend/src/main/java/salt/dev/saltidcard/controller/Controller.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.*;
 import salt.dev.saltidcard.controller.dtos.UserDto;
 import salt.dev.saltidcard.service.IdCardService;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("api/v1/id-card")
 @CrossOrigin
@@ -16,7 +18,17 @@ public class Controller {
     }
 
     @GetMapping("{email}")
-    public UserDto getIdCard(@PathVariable String email) {
+    public UserDto getIdCardFromEmail(@PathVariable String email) {
         return UserDto.toModel(service.getIdCard(email));
+    }
+
+    @GetMapping("{privateEmail}")
+    public UserDto getIdCardFromPrivateEmail(@PathVariable String privateEmail) {
+        return UserDto.toModel(service.getIdCard(privateEmail));
+    }
+
+    @GetMapping("{uuid}")
+    public UserDto getIdCardFromUuid(@PathVariable UUID uuid) {
+        return UserDto.toModel(service.getIdCard(uuid));
     }
 }
