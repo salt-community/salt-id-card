c0a85552c27fbf746a7f773fc1d0b1fb9b6d305c
TobiasBlankJohansson <Tobias@annito.se>
2025-02-26 16:24:24 +0100

    feat::add fetch to backend in getSaltDataUuid

--- frontend/src/api/notion.ts
+++ frontend/src/api/notion.ts
@@ -6,7 +6,7 @@ export async function getSaltDataEmail(
   email: string | undefined
 ): Promise<User> {
   return {
-    uuid: "4c51571d-7244-478c-9fff-9ec8941f0094",
+    uuid: "02e93524-a7a9-4f63-9e2e-aa5b729ce398",
     name: "Tobias Johansson",
     course: "jfs-sthlm-2026-09-06",
     endDate: "2025-06-01",
@@ -21,6 +21,11 @@ export async function getSaltDataEmail(
 }
 
 export async function getSaltDataUuid(uuid: string | undefined): Promise<User> {
+  const result = await fetch(BACKEND_PATH + "uuid/" + uuid);
+  if (!result.ok) {
+    throw new Error();
+  }
+  return (await result.json()) as User;
   return {
     uuid: "4c51571d-7244-478c-9fff-9ec8941f0094",
     name: "Tobias Johansson",
@@ -29,9 +34,4 @@ export async function getSaltDataUuid(uuid: string | undefined): Promise<User> {
     email: "tobias.johansson@appliedtechnology.se",
     image: "https://avatars.githubusercontent.com/TobiasBlankJohansson",
   };
-  const result = await fetch(BACKEND_PATH + "uuid/" + uuid);
-  if (!result.ok) {
-    throw new Error();
-  }
-  return (await result.json()) as User;
 }
