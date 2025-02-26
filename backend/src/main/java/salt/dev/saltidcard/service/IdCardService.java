3e676a9d20498fd57476423116f60968d475ce2b
TobiasBlankJohansson <Tobias@annito.se>
2025-02-26 15:45:36 +0100

    feat:add logit to return a new user from userDto

--- backend/src/main/java/salt/dev/saltidcard/service/IdCardService.java
+++ backend/src/main/java/salt/dev/saltidcard/service/IdCardService.java
@@ -6,6 +6,8 @@ import salt.dev.saltidcard.notionproxy.NotionProxyService;
 import salt.dev.saltidcard.notionproxy.dtos.UserNotionProxyDto;
 
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class IdCardService {
@@ -27,11 +29,28 @@ public class IdCardService {
 
     public User getIdCardUuid(UUID uuid){
         UserNotionProxyDto userDto = notionProxyService.fetchUser("id-cards/uuid/" + uuid.toString());
-        return new User("4c51571d-7244-478c-9fff-9ec8941f0094",
-                "Tobias Johansson",
-                "jfs-sthlm-2024-09-06",
-                "2026-09-06",
-                "tobias.johansson@appliedtechnology.se",
-                "https://avatars.githubusercontent.com/TobiasBlankJohansson");
+
+        Pattern pattern = Pattern.compile("(\\d{4})-(\\d{2})-(\\d{2})");
+        Matcher matcher = pattern.matcher(userDto.getCourse());
+        matcher.find();
+        int year = Integer.parseInt(matcher.group(1)) + 2;
+        String endDate = year + "-" + matcher.group(2) + "-" + matcher.group(3);
+        return new User(userDto.getUuid(),
+                userDto.getName(),
+                userDto.getCourse(),
+                endDate,
+                userDto.getEmail(),
+                getGitHubAvatarUrl(userDto.getGitHub()));
+
+    }
+
+    public static String getGitHubAvatarUrl(String profileUrl) {
+        if (profileUrl == null || !profileUrl.startsWith("https://github.com/")) {
+            throw new IllegalArgumentException("Invalid GitHub profile URL");
+        }
+
+        String username = profileUrl.replace("https://github.com/", "").trim();
+
+        return "https://avatars.githubusercontent.com/" + username;
     }
 }
