a065ce161f741212efcd70614b648753a85925a3
TobiasBlankJohansson <Tobias@annito.se>
2025-02-26 11:14:04 +0100

    feat:add hook useSaltData

---
+++ src/hooks/useSaltData/useSaltData.ts
@@ -0,0 +1,29 @@
+import { useCallback, useState } from "react";
+import { User } from "../../types";
+import { getSaltDataEmail } from "../../api/notion";
+
+export const useSaltData = (email?: string) => {
+  const [userData, setUserData] = useState<User | null>(null);
+  const [error, setError] = useState<boolean>(false);
+
+  const getSaltData = useCallback(async () => {
+    if (email) {
+      try {
+        const data = await getSaltDataEmail(email);
+        setUserData({
+          uuid: data.uuid,
+          email: data.email,
+          name: data.name,
+          course: data.course,
+          endDate: data.endDate,
+          image: data.image,
+        });
+        setError(false);
+      } catch {
+        setError(true);
+      }
+    }
+  }, [email]);
+
+  return { userData, error, getSaltData };
+};
