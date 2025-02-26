b65617620bf36ac881704cd8ea4632585cf88bf4
TobiasBlankJohansson <Tobias@annito.se>
2025-02-26 15:24:11 +0100

    refa:refactor UserNotionProxyDto by removing unneeded code

--- backend/src/main/java/salt/dev/saltidcard/notionproxy/dtos/UserNotionProxyDto.java
+++ backend/src/main/java/salt/dev/saltidcard/notionproxy/dtos/UserNotionProxyDto.java
@@ -1,8 +1,5 @@
 package salt.dev.saltidcard.notionproxy.dtos;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,14 +9,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserNotionProxyDto {
-
     private String uuid, name, course, email, gitHub;
-
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    public static UserNotionProxyDto fromJson(String json) throws JsonProcessingException {
-        return objectMapper.readValue(json, UserNotionProxyDto.class);
-    }
 }
