b708eba4d3f9d73df043d6764f7cb39ab955ee1e
TobiasBlankJohansson <Tobias@annito.se>
2025-02-26 14:51:25 +0100

    refa:refactor organisation to frontend and backend

---
+++ frontend/src/types.ts
@@ -0,0 +1,8 @@
+export type User = {
+  uuid: string;
+  name: string;
+  course: string;
+  endDate: string;
+  email: string | undefined;
+  image: string;
+};
--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export type User = {
-  uuid: string;
-  name: string;
-  course: string;
-  endDate: string;
-  email: string | undefined;
-  image: string;
-};
