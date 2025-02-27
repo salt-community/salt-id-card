dc79f77dbdcb70227ca97576048da752fb82d2b1
TobiasBlankJohansson <Tobias@annito.se>
2025-02-27 14:01:43 +0100

    feat:add errorMessage in EmailNotFoundModal

--- frontend/src/components/email-not-found-modal/email-not-found-modal.tsx
+++ frontend/src/components/email-not-found-modal/email-not-found-modal.tsx
@@ -1,27 +1,32 @@
 import { CtaButton } from "../button";
+import { InfoBox } from "../info-box";
 import "./email-not-found-modal.css";
 import { SignOutButton } from "@clerk/clerk-react";
 
 type EmailNotFoundModalProps = {
+  errorMessage: string;
   onConfirm: () => Promise<void>;
 };
 
 export default function EmailNotFoundModal({
+  errorMessage,
   onConfirm,
 }: EmailNotFoundModalProps) {
   return (
     <div className="modal-overlay">
       <div className="modal">
         <h2>Email Not Found</h2>
-        <p>
-          We couldn't find your email in the Notion database. Please speak with
+        <InfoBox>
+          {errorMessage}. Please speak with
           your instructor.
-        </p>
+        </InfoBox>
         <div className="modal-footer">
           <SignOutButton>
             <CtaButton variant="info">Cancel</CtaButton>
           </SignOutButton>
-          <CtaButton variant="primary" onClick={() => onConfirm()}>Try again</CtaButton>
+          <CtaButton variant="primary" onClick={() => onConfirm()}>
+            Try again
+          </CtaButton>
         </div>
       </div>
     </div>
