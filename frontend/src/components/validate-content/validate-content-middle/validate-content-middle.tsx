9d414521a881b80624ed9daad5ad1f32709c768a
TobiasBlankJohansson <Tobias@annito.se>
2025-02-27 11:32:21 +0100

    faeta.add error handeling in validate-content-middle

--- frontend/src/components/validate-content/validate-content-middle/validate-content-middle.tsx
+++ frontend/src/components/validate-content/validate-content-middle/validate-content-middle.tsx
@@ -6,17 +6,21 @@ import { useSaltDataId } from "../../../hooks/use-salt-data-id";
 import { Loading } from "../../loading";
 
 export const ValidateContentMiddle = () => {
-  const { userData, getSaltData } = useSaltDataId();
+  const { userData, error, getSaltData } = useSaltDataId();
   useEffect(() => {
     getSaltData();
   }, [getSaltData]);
 
   return (
     <section className="validate__section-middle">
-      {userData ? (
+      {error ? (
+        <></>
+      ) : userData ? (
         <div className="validate__section-middle__wrapper">
-          <Title content="Hello! It looks like you've found my ID card." />
-          <Profile userData={userData} />
+          <>
+            <Title content="Hello! It looks like you've found my ID card." />
+            <Profile userData={userData} />
+          </>
         </div>
       ) : (
         <Loading />
