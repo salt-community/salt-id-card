40fea1a22fc79efb50490828c9c110ebc1b4943d
TobiasBlankJohansson <Tobias@annito.se>
2025-02-27 09:16:01 +0100

    feat:add handleException for unpredicted exeptions

--- backend/src/main/java/salt/dev/saltidcard/exception/GlobalHandler.java
+++ backend/src/main/java/salt/dev/saltidcard/exception/GlobalHandler.java
@@ -11,4 +11,9 @@ public class GlobalHandler {
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(e.getMessage());
     }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
 }
