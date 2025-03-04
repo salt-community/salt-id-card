e727607116f92ea2b81965d570745b4833479524
TobiasBlankJohansson <Tobias@annito.se>
2025-03-04 10:59:39 +0100

    feat:add better error messages

--- backend/src/main/java/salt/dev/saltidcard/user/service/IdCardServiceConstants.java
+++ backend/src/main/java/salt/dev/saltidcard/user/service/IdCardServiceConstants.java
@@ -1,11 +1,11 @@
 package salt.dev.saltidcard.user.service;
 class IdCardServiceConstants {
-    public static final String ERROR_UUID = "Invalid uuid";
-    public static final String ERROR_NAME = "Invalid name";
-    public static final String ERROR_COURSE = "Invalid course";
-    public static final String ERROR_EMAIL = "Invalid email";
-    public static final String ERROR_END_DATE = "Invalid end date";
-    public static final String ERROR_GITHUB = "Invalid GitHub profile URL";
+    public static final String ERROR_UUID = "Invalid uuid from notion proxy";
+    public static final String ERROR_NAME = "Invalid name from notion proxy";
+    public static final String ERROR_COURSE = "Invalid course from notion proxy";
+    public static final String ERROR_EMAIL = "Invalid email from notion proxy";
+    public static final String ERROR_END_DATE = "Invalid end date from notion proxy";
+    public static final String ERROR_GITHUB = "Invalid GitHub profile URL from notion proxy";
 
     public static final String NULL = "null";
     public static final String GITHUB_URL = "https://github.com/";
