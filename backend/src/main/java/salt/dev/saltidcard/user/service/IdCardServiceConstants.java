4415ce64f6d798c7780748e2ff12afa04f1aeb4a
TobiasBlankJohansson <Tobias@annito.se>
2025-03-04 10:54:53 +0100

    feat:add static strings for Utils strings

--- backend/src/main/java/salt/dev/saltidcard/user/service/IdCardServiceConstants.java
+++ backend/src/main/java/salt/dev/saltidcard/user/service/IdCardServiceConstants.java
@@ -6,4 +6,8 @@ class IdCardServiceConstants {
     public static final String ERROR_EMAIL = "Invalid email";
     public static final String ERROR_END_DATE = "Invalid end date";
     public static final String ERROR_GITHUB = "Invalid GitHub profile URL";
+
+    public static final String NULL = "null";
+    public static final String GITHUB_URL = "https://github.com/";
+    public static final String IMAGE_URL = "https://avatars.githubusercontent.com/";
 }
--- backend/src/main/java/salt/dev/saltidcard/user/service/Utils.java
+++ backend/src/main/java/salt/dev/saltidcard/user/service/Utils.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 import static salt.dev.saltidcard.user.service.IdCardServiceConstants.*;
 
 public class Utils {
+
     public static String getEndDate(String startDate) {
         Pattern pattern = Pattern.compile("(\\d{4})-(\\d{2})-(\\d{2})");
         Matcher matcher = pattern.matcher(startDate);
@@ -16,17 +17,17 @@ public class Utils {
     }
 
     public static String ifNotEmptyReturn(String input, String errorMessage){
-        if(input == null || input.equals("null") || input.isEmpty()){
+        if(input == null || input.equals(NULL) || input.isEmpty()){
             throw new IllegalArgumentException(errorMessage);
         }
         return input;
     }
 
     public static String getGitHubAvatarUrl(String profileUrl) {
-        if (profileUrl == null || !profileUrl.startsWith("https://github.com/")) {
+        if (profileUrl == null || !profileUrl.startsWith(GITHUB_URL)) {
             throw new IllegalArgumentException(ERROR_GITHUB);
         }
-        String username = profileUrl.replace("https://github.com/", "").trim();
-        return "https://avatars.githubusercontent.com/" + username;
+        String username = profileUrl.replace(GITHUB_URL, "").trim();
+        return IMAGE_URL + username;
     }
 }
